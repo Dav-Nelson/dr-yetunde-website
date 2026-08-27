d4cfad25bb64bc4563a6a02ab80789121215f3db
David Nelson <med.nelsondavid@gmail.com>
2026-08-28 00:11:33 +0100

    Add BlogPost and Login routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import LoginTest from './pages/LoginTest';
 
 // Admin
 import AdminLayout from './components/layout/AdminLayout';
@@ -9,13 +8,14 @@ import AdminDashboard from './pages/admin/Dashboard';
 import BlogManagement from './pages/admin/BlogManagement';
 import SpeakingManagement from './pages/admin/SpeakingManagement';
 
-
 // Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
 import Speaking from './pages/Speaking';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -28,16 +28,16 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
             <Route path="/speaking" element={<Speaking />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/login-test" element={<LoginTest />} />
-            <Route element={<AdminLayout />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route element={<AdminLayout />}>
               <Route path="/admin" element={<AdminDashboard />} />
               <Route path="/admin/blog" element={<BlogManagement />} />
               <Route path="/admin/speaking" element={<SpeakingManagement />} />
-              {/* Later:
-                <Route path="/admin/blog" element={<AdminDashboard />} />
-              */}
+            </Route>
           </Routes>
         </main>
 
